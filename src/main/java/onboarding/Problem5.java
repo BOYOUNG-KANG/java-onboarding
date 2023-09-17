d2d44b76b0913e881ad089daf668d39d4fe1af2c
BOYOUNG-KANG <28287@naver.com>
2023-09-17 17:22:41 +0900

    feat: problem5 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,42 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        validate(money);
+        Map<Integer, Integer> moneyDirectory = generateMoneyDirectory();
+
+        int leftMoney = money;
+        countMoney(money, answer, moneyDirectory, leftMoney);
         return answer;
+    }
+
+    private static Map<Integer, Integer> generateMoneyDirectory() {
+        Map<Integer, Integer> moneyDirectory = new HashMap<>();
+        moneyDirectory.putAll(Map.of(
+                0, 50000,
+                1, 10000,
+                2, 5000,
+                3, 1000,
+                4, 500,
+                5, 100,
+                6, 50,
+                7, 10,
+                8, 1
+        ));
+        return moneyDirectory;
+    }
+
+    private static void countMoney(int money, List<Integer> answer, Map<Integer, Integer> moneyDirectory, int leftMoney) {
+        for (int i = 0; i < moneyDirectory.size(); i++) {
+            answer.add(i, leftMoney / moneyDirectory.get(i));
+            leftMoney = money % moneyDirectory.get(i);
+        }
+    }
+
+    private static void validate(int money){
+        if (money < 1 || money > 1000000) throw new IllegalArgumentException();
     }
 }
